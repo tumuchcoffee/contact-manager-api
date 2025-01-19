8a2110e9116f5ff88531798b7ea1c4ba22333666
EricJDotNet <ericjones.net@gmail.com>
2025-01-19 10:16:04 -0500

    Migrate to elastic v8 client.

---
+++ ContactManager.Data/ElasticDataLayer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+
+namespace ContactManager.Data
+{
+	public class ElasticDataLayer<T> where T : class
+	{
+		public readonly ElasticsearchClient _client;
+
+		/// <summary>
+		/// Initializes a new instance of the ElasticsearchDataLayer class.
+		/// </summary>
+		/// <param name="uri">The URI of the Elasticsearch server.</param>
+		public ElasticDataLayer(Uri uri, string indexName)
+		{
+			var settings = new ElasticsearchClientSettings(uri)
+				.DefaultIndex(indexName);
+
+			_client = new ElasticsearchClient(settings);
+		}
+
+		/// <summary>
+		/// Indexes a document in Elasticsearch.
+		/// </summary>
+		/// <param name="document">The document to index.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the index response.</returns>
+		public async Task<IndexResponse> IndexDocumentAsync(T document)
+		{
+			return await _client.IndexAsync(document);
+		}
+
+		/// <summary>
+		/// Searches for documents based on a query.
+		/// </summary>
+		/// <param name="query">The query string for searching.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the search response.</returns>
+		public async Task<SearchResponse<T>> SearchAsync(string query)
+		{
+			var searchResponse = await _client.SearchAsync<T>(s => s
+				.Query(q => q
+					.QueryString(d => d
+						.Query(query)
+					)
+				)
+			);
+			return searchResponse;
+		}
+
+		/// <summary>
+		/// Deletes a document by its ID.
+		/// </summary>
+		/// <param name="id">The ID of the document to delete.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the delete response.</returns>
+		public async Task<DeleteResponse> DeleteDocumentAsync(string id)
+		{
+			return await _client.DeleteAsync<T>(id);
+		}
+
+		/// <summary>
+		/// Retrieves a document by its ID.
+		/// </summary>
+		/// <param name="id">The ID of the document to retrieve.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the get document response.</returns>
+		public async Task<GetResponse<T>> GetDocumentAsync(string id)
+		{
+			return await _client.GetAsync<T>(id);
+		}
+
+		/// <summary>
+		/// Updates a document in Elasticsearch.
+		/// </summary>
+		/// <param name="id">The ID of the document to update.</param>
+		/// <param name="document">The updated document.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the update response.</returns>
+		public async Task<UpdateResponse<T>> UpdateDocumentAsync(string id, T document)
+		{
+			return await _client.UpdateAsync<T, T>(id, u => u
+				.Doc(document)
+			);
+		}
+	}
+}
---
+++ ContactManager.Service/ContactService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManager.Domain;
+using ContactManager.Data;
+using Elastic.Clients.Elasticsearch;
+
+namespace ContactManager.Service
+{
+	public class ContactService
+	{
+		private readonly ElasticDataLayer<Contact> _elasticDataLayer;
+
+		public ContactService(Uri elasticsearchUri)
+		{
+			_elasticDataLayer = new ElasticDataLayer<Contact>(elasticsearchUri, "contacts");
+		}
+
+		/// <summary>
+		/// Retrieves all contacts from the "contacts" index in Elasticsearch.
+		/// </summary>
+		/// <returns>A task that represents the asynchronous operation. The task result contains a list of contacts.</returns>
+		public async Task<List<Contact>> GetAllContactsAsync()
+		{
+			var searchResponse = await _elasticDataLayer.SearchAsync("{ 'query': { 'match_all': {} } }");
+
+			if (!searchResponse.IsValidResponse)
+			{
+				throw new Exception($"Elasticsearch search failed: {searchResponse.DebugInformation}");
+			}
+
+			return searchResponse.Documents.ToList();
+		}
+
+		/// <summary>
+		/// Retrieves a contact by its ID from Elasticsearch.
+		/// </summary>
+		/// <param name="id">The ID of the contact to retrieve.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the contact if found, or null if not found.</returns>
+		public async Task<Contact?> GetContactByIdAsync(string id)
+		{
+			var response = await _elasticDataLayer.GetDocumentAsync(id);
+
+			if (!response.IsValidResponse)
+			{
+				return null; // Contact not found or operation failed
+			}
+
+			return response.Source;
+		}
+
+		/// <summary>
+		/// Creates a new contact in Elasticsearch.
+		/// </summary>
+		/// <param name="contact">The contact object to create.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the contact with its ID assigned by Elasticsearch.</returns>
+		public async Task<Contact> CreateContactAsync(Contact contact)
+		{
+			var response = await _elasticDataLayer.IndexDocumentAsync(contact);
+
+			if (!response.IsValidResponse)
+			{
+				throw new Exception($"Failed to create contact: {response.DebugInformation}");
+			}
+
+			// Update the contact with the ID returned by Elasticsearch
+			contact.Id = response.Id;
+			return contact;
+		}
+
+		/// <summary>
+		/// Updates an existing contact in Elasticsearch.
+		/// </summary>
+		/// <param name="contact">The contact object to update, including its ID.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result is true if the update was successful, false otherwise.</returns>
+		public async Task<bool> UpdateContactAsync(Contact contact)
+		{
+			if (string.IsNullOrEmpty(contact.Id))
+			{
+				throw new ArgumentException("Contact ID must be provided for updates.");
+			}
+
+			var response = await _elasticDataLayer.UpdateDocumentAsync(contact.Id, contact);
+
+			if (!response.IsValidResponse)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
